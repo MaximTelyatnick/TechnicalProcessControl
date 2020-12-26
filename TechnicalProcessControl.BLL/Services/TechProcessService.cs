4b52d8756a6421ae784c4daf2b37d5c16503fd1a
Maxim <maxtel011395@gmail.com>
2020-12-26 23:49:54 +0200

    Add TechProcessService

---
+++ TechnicalProcessControl.BLL/Services/TechProcessService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalProcessControl.BLL.Interfaces;
+
+namespace TechnicalProcessControl.BLL.Services
+{
+    public class TechProcessService : ITechProcessService
+    {
+
+
+
+    }
+}
